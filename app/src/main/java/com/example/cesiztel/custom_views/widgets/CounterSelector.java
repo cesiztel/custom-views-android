8f8ba1b4fe4873ccb95a402ddeaefdd6adb20ab6
Cesar Izquierdo Tello <cesar.izquierdo.tello@CodeCodings-MacBook-Pro.local>
2017-03-09 22:52:34 +0100

    Sample of other component how to do it more generic passing attributes to the compound view and set this attributes to the compound view inflated. In this case, change the like selector to more generic counter selector where is possible add/substract likes or other items and changing the thumb image for other images (for instance a + or - simbol)

---
+++ app/src/main/java/com/example/cesiztel/custom_views/widgets/CounterSelector.java
@@ -0,0 +1,104 @@
+package com.example.cesiztel.custom_views.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.cesiztel.custom_views.R;
+
+public class CounterSelector extends LinearLayout {
+    private Drawable mDrawableDown;
+    private Drawable mDrawableUp;
+    private ImageView mUp;
+    private ImageView mDown;
+    private TextView mCounter;
+
+    public CounterSelector(Context context) {
+        super(context);
+        initializeViews(context);
+    }
+
+    public CounterSelector(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray attributes;
+        attributes = context.
+                obtainStyledAttributes(attrs, R.styleable.CounterSelector);
+        mDrawableUp = attributes.getDrawable(R.styleable.CounterSelector_drawableUp);
+        mDrawableDown = attributes.getDrawable(R.styleable.CounterSelector_drawableDown);
+
+        attributes.recycle();
+
+        initializeViews(context);
+    }
+
+    public CounterSelector(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray attributes;
+        attributes = context.
+                obtainStyledAttributes(attrs, R.styleable.CounterSelector);
+        mDrawableUp = attributes.getDrawable(R.styleable.CounterSelector_drawableUp);
+        mDrawableDown = attributes.getDrawable(R.styleable.CounterSelector_drawableDown);
+
+        attributes.recycle();
+
+        initializeViews(context);
+    }
+
+    /**
+     * Inflates de views in the layout
+     *
+     * @param context
+     *          the current context for the view
+     */
+    private void initializeViews(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.counter_view, this);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        // Sets the resources
+        mUp = (ImageView) this
+                .findViewById(R.id.up_selector);
+        mDown = (ImageView) this
+                .findViewById(R.id.down_selector);
+        mCounter = (TextView) this
+                .findViewById(R.id.counter_text_view);
+
+        // Set the drawer resources
+        mUp.setImageDrawable(mDrawableUp);
+        mDown.setImageDrawable(mDrawableDown);
+
+        // Add listener/interactions
+        mUp.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int likes = Integer.valueOf(mCounter.getText().toString());
+                likes++;
+                mCounter.setText(String.valueOf(likes));
+            }
+        });
+
+        mDown.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int likes = Integer.valueOf(mCounter.getText().toString());
+                if(likes == 0) return;
+                likes--;
+                mCounter.setText(String.valueOf(likes));
+            }
+        });
+    }
+}
